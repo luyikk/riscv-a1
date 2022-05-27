b07ea39b46fe4f367a0f4fef9810abe83c55d04f
luyikk <luyikk@126.com>
2022-05-27 18:34:39 +0800

    init

---
+++ core/src/panic_def.rs
@@ -0,0 +1,19 @@
+
+use core::panic::PanicInfo;
+use crate::{error};
+use crate::sbi::shutdown;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        error!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        error!("Panicked: {}", info.message().unwrap());
+    }
+    shutdown()
+}
